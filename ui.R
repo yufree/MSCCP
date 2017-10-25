42de08807f072bc524f2d21a757243ff3b37f545
Yufree <yufree@live.cn>
2017-10-24 21:55:11 -0400

    update ui

--- ui.R
+++ ui.R
@@ -166,5 +166,14 @@ shinyUI(navbarPage(
         )),
         tabPanel("Ions for analysis",
                  dataTableOutput("data")),
-        tabPanel("References")
+        tabPanel("References",
+                 p("Bogdal, C., Alsberg, T., Diefenbacher, P.S., MacLeod, M., Berger, U., 2015. Fast Quantification of Chlorinated Paraffins in Environmental Samples by Direct Injection High-Resolution Mass Spectrometry with Pattern Deconvolution. Anal. Chem. 87, 2852–2860. doi:10.1021/ac504444d"),
+                 p("Gao, W., Wu, J., Wang, Y., Jiang, G., 2016. Quantification of short- and medium-chain chlorinated paraffins in environmental samples by gas chromatography quadrupole time-of-flight mass spectrometry. Journal of Chromatography A 1452, 98–106. doi:10.1016/j.chroma.2016.04.081"),
+                 p("ISO 12010:2012 - Water quality -- Determination of short-chain polychlorinated alkanes (SCCPs) in water -- Method using gas chromatography-mass spectrometry (GC-MS) and negative-ion chemical ionization (NCI) [WWW Document], n.d. URL https://www.iso.org/standard/51124.html (accessed 10.25.17)."),
+                 p("Nilsson, M.-L., Bengtsson, S., Kylin, H., 2012. Identification and determination of chlorinated paraffins using multivariate evaluation of gas chromatographic data. Environmental Pollution 163, 142–148. doi:10.1016/j.envpol.2011.12.010"),
+                 p("Rusina, T., Korytar, P., Boer, J. de, 2011. Comparison of quantification methods for the analysis of polychlorinated alkanes using electron capture negative ionization mass spectrometry. Intern J Environ Anal Chem, INT J ENVIRON AN CH, Intern. J. Environ. Anal. Chem. 91, 319–332. doi:10.1080/03067311003602583"),
+                 p("Tomy, G.T., Stern, G.A., Muir, D.C.G., Fisk, A.T., Cymbalisty, C.D., Westmore, J.B., 1997. Quantifying C10−C13 Polychloroalkanes in Environmental Samples by High-Resolution Gas Chromatography/Electron Capture Negative Ion High-Resolution Mass Spectrometry. Anal. Chem. 69, 2762–2771. doi:10.1021/ac961244y"),
+                 p("van Mourik, L.M., Leonards, P.E.G., Gaus, C., de Boer, J., 2015. Recent developments in capabilities for analysing chlorinated paraffins in environmental matrices: A review. Chemosphere 136, 259–272. doi:10.1016/j.chemosphere.2015.05.045"),
+                 p("Yuan, B., Bogdal, C., Berger, U., MacLeod, M., Gebbink, W.A., Alsberg, T., de Wit, C.A., 2017. Quantifying Short-Chain Chlorinated Paraffin Congener Groups. Environ. Sci. Technol. 51, 10633–10641. doi:10.1021/acs.est.7b02269")
+)
 ))
